52074762922110809e8e8024f4988a38c3af32ac
Khai Truong <KTruong@absolute.com>
2019-11-20 14:12:18 +0700

    Refactory splitMessage by create some function to control how to change Indicator and Message parts

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-loop-func */
 import { ACCEPTED_MAXIMUM_CHARS } from './globalConstants';
+import { __values } from 'tslib';
 
 export const checkExistedWordOverAcceptedChars = (message) => {
   const regex = new RegExp(`([\\S]){${ACCEPTED_MAXIMUM_CHARS},}`,'g')
@@ -13,11 +14,26 @@ const PartIndicator = (totalMessageParts) => {
   let indicator = 0;
   let partIndicator = !!totalMessageParts ? `${indicator}/${totalMessageParts}`:'';
   return {
-    getString() { return partIndicator },
+    getValue() { return partIndicator },
     increaseIndicator() {
       indicator++;
       partIndicator = !!totalMessageParts ? `${indicator}/${totalMessageParts}`:'';
     },
+  }
+}
+
+const MessageParts = () => {
+  let parts = [];
+  return {
+    clear() { parts.length = 0 },
+    add(partIndicatorValue, words, startIndexOfWords, endIndexOfWords) { 
+      parts.push([
+        partIndicatorValue,
+        ...words.slice(startIndexOfWords, endIndexOfWords)
+      ].join(' '))
+    },
+    getValue() { return parts },
+    length() { return parts.length }
   }
 }
 
@@ -31,17 +47,17 @@ export const splitMessage = (inputMessage) => {
   const nonWhiteSpaceWords = inputMessage.split(' ');
 
   let totalMessageParts = -1;
-  let messageParts = [];
+  let messageParts = MessageParts();
 
-  while (messageParts.length !== totalMessageParts) {
-    totalMessageParts = messageParts.length;
+  while (messageParts.length() !== totalMessageParts) {
+    totalMessageParts = messageParts.length();
 
     // Ensure messageParts is empty for re-calculate
-    messageParts.length = 0;
+    messageParts.clear();
 
     let partIndicator = PartIndicator(totalMessageParts);
 
-    let counter = partIndicator.getString().length;
+    let counter = partIndicator.getValue().length;
     let startWordIndex = 0;
 
     nonWhiteSpaceWords.forEach((word, index) => {
@@ -50,31 +66,31 @@ export const splitMessage = (inputMessage) => {
         // Pickup a word but it make counter > 56
         // So we will return back this word, and reset counter,
         // The counter should be start equal word.length (which be returned);
-        counter = word.length;
-        const messagePart = nonWhiteSpaceWords.slice(startWordIndex, index);
         partIndicator.increaseIndicator();
-        messageParts.push([partIndicator.getString(), ...messagePart].join(' '));
+        messageParts.add(partIndicator.getValue(), nonWhiteSpaceWords, startWordIndex, index);
+
         startWordIndex = index;
-        
+        counter = word.length;
       }
       if (counter === ACCEPTED_MAXIMUM_CHARS) {
         counter = 0;
-        const messagePart = nonWhiteSpaceWords.slice(startWordIndex, index + 1);
         partIndicator.increaseIndicator();
-        messageParts.push([partIndicator.getString(), ...messagePart].join(' '));
+        messageParts.add(partIndicator.getValue(), nonWhiteSpaceWords, startWordIndex, index + 1);
+
+        counter = 0;
         startWordIndex = index;
       }
       if (index === nonWhiteSpaceWords.length -1) {
         // When counter < 50 but don't have any word to check
         // Add it as a final message part;
-        const messagePart = nonWhiteSpaceWords.slice(startWordIndex, index + 1);
         partIndicator.increaseIndicator();
-        messageParts.push([partIndicator.getString(), ...messagePart].join(' '));
+        messageParts.add(partIndicator.getValue(), nonWhiteSpaceWords, startWordIndex, index + 1);
+
         counter = 0;
         startWordIndex = 0;
       }
     })
   }
 
-  return messageParts;
+  return messageParts.getValue();
 }
